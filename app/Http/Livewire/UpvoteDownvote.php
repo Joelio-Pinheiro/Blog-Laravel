3b4008e151cc778627c40a4f687a4f7c179e6e9d
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2024-08-14 16:05:19 -0300

    feat: add UpvoteDownVote integration

--- app/Http/Livewire/UpvoteDownvote.php
+++ app/Http/Livewire/UpvoteDownvote.php
@@ -23,7 +23,20 @@ class UpvoteDownvote extends Component
         ->where('is_upvote', '=', false)
         ->count();
 
-        return view('livewire.upvote-downvote', compact('upvotes', 'downvotes'));
+        $hasUpvote = null;
+
+        /** @var \App\Models\User $user */
+        $user = request()->user();
+        if ($user) {
+            $model = \App\Models\UpvoteDownvote::where('post_id', '=', $this->post->id)
+            ->where('user_id', '=', $user->id)
+            ->first();
+            if ($model) {
+                $hasUpvote = !!$model->is_upvote;
+            }
+        }
+
+        return view('livewire.upvote-downvote', compact('upvotes', 'downvotes', 'hasUpvote'));
     }
 
     public function upvoteDownvote($upvote = true){
@@ -39,6 +52,25 @@ class UpvoteDownvote extends Component
             return $this->redirect(route('verification.notice'));
         }
 
-        // $upvoteDownvote = \App\Models\
+        $model = \App\Models\UpvoteDownvote::where('post_id', '=', $this->post->id)
+        ->where('user_id', '=', $user->id)
+        ->first();
+
+        if (!$model) {
+            \App\Models\UpvoteDownvote::create([
+                'is_upvote' => $upvote,
+                'post_id' => $this->post->id,
+                'user_id' => $user->id
+            ]);
+
+            return ;
+        }
+
+        if ($upvote && $model->is_upvote || !$upvote && !$model->is_upvote) {
+            $model->delete();
+        } else {
+            $model->is_upvote = $upvote;
+            $model->save();
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\TextWidget;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,15 +17,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Post::factory(50)->create();
-        /*$adminUser = User::factory()-> create([
-            'email' => 'admin3@teste.com',
-            'name' => 'admin3',
-            'password' => bcrypt('12345678')
-        ]);
+        // \App\Models\Post::factory(50)->create();
+        // $adminUser = User::factory()->create([
+        //     'email' => 'admin3@teste.com',
+        //     'name' => 'admin3',
+        //     'password' => bcrypt('12345678')
+        // ]);
 
-        $adminRole = Role::firstOrCreate(['name' => 'admin']);
-        $adminUser->assignRole($adminRole);*/
+        // $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        // $adminUser->assignRole($adminRole);
+
+
+        $faker = FakerFactory::create();
+        $widgetsConfig = [
+            [
+                'key' => 'about-us-sidebar',
+                'title' => 'Sobre mim',
+                'Content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos sequi iste exercitationem at accusamus a voluptatem, aliquam, velit cupiditate qui cumque, delectus culpa. Minus fuga minima cumque totam id nihil.',
+                'active' => true
+            ],
+            [
+                'key' => 'head',
+                'title' => 'Lorem Ipsum Dolor Sit Amet',
+                'active' => true
+            ],
+            [
+                'key' => 'about_page',
+                'title' => 'About Us',
+                'content' => 'Todos os arquivos de configuração do framework Laravel estão armazenados no diretório config. Cada opção está documentada, então sinta-se à vontade para revisar os arquivos e se familiarizar com as opções disponíveis.
+                Se um banco de dados SQLite não existir para sua aplicação, o Laravel perguntará se você deseja que o banco de dados seja criado. Normalmente, o arquivo de banco de dados SQLite será criado em database/database.sqlite.',
+                'active' => true,
+                'image' =>  $faker->imageUrl(640, 480, 'cats', true),
+            ]
+        ];
+        $widgets = [];
+
+        foreach($widgetsConfig as $config) {
+            $widgets[] = TextWidget::factory()->create($config);
+        }
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
