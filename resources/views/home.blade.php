0b6ad18c70244dcb2589ff65fc0664fc70bb48d7
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2024-08-30 14:03:25 -0300

    fix: format page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,12 @@
 ?>
 
 <x-app-layout meta-title="Blog UFC" meta-description="Meu Blog pessoal da UFC">
-    <div class="container max-w-4xl mx-auto py-6">
+    <div class="container max-w-6xl mx-auto py-6">
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
 
             {{-- Latest post --}}
-            <div class="col-span-2">
+            <div class="col-span-3 sm:col-span-2">
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
                     Latest Post
                 </h2>
@@ -17,7 +17,7 @@
             </div>
 
             {{-- Popular 3 Post --}}
-            <div class="col-span-1">
+            <div>
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
                     Popular Posts
                 </h2>
@@ -65,21 +65,23 @@
             </div>
 
             {{-- Latest Categories --}}
-            <div class="col-span-3">
-                <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
-                    Recent Categories
-                </h2>
-                @foreach ($categories as $category)
+            @foreach ($categories as $category)
+                <div class="col-span-3">
+                    <h2
+                        class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
+                        <a href="{{route('by-category', $category)}}">
+                            Category "{{ $category->title }}" <i class="fas fa-arrow-right"></i>
+                        </a>
+                    </h2>
                     <div class="mb-6">
-                        <h3 class="text-xl font-bold text-center">{{ $category->title }}</h3>
                         <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
                             @foreach ($category->publishedPosts()->limit(3)->get() as $post)
-                                <x-post-item :post="$post" :show-author="false"/>
+                                <x-post-item :post="$post" :show-author="false" />
                             @endforeach
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
 
     </div>
